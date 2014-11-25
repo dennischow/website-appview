cc611a7955def4a2e8ed5dbb5f0b71f588594713
Fat-Cow <dennischow07@gmail.com>
2014-11-25 18:02:54 +0800

    Angular Customer List Applied

--- assets/js/app/app.js
+++ assets/js/app/app.js
@@ -1,1 +1,10 @@
 console.log('This is an App');
+
+var app = angular.module('customersApp', []);
+
+app.controller('CustomersController', function($scope){
+
+	$scope.customers = customerData; // Referal to /api/data.js
+
+});
+
